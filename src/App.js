dec21e36fb9b9697f38f0f87b52f8abc2b682cf5
michellele994 <michellele@email.arizona.edu>
2018-05-03 16:08:29 -0700

    Grass border was ugly on pigs and up until eggs. Fixed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import Pictures from "./components/Pictures";
 import Scores from "./components/Scores";
 
 
-const loadPictures = function(typeOfPicture, number)
-  {
-    let array = [];
-    for (let i = 1; i < number+1; i++)
-    {
-      let picture = {
-        id: i,
-        image: "./images/"+typeOfPicture+i+".png"
-      }
-      array.push(picture);
+const loadPictures = function (typeOfPicture, number) {
+  let array = [];
+  for (let i = 1; i < number + 1; i++) {
+    let picture = {
+      id: i,
+      image: "./images/" + typeOfPicture + i + ".png"
     }
-    return array;
+    array.push(picture);
   }
+  return array;
+}
 //array of 8 cows, 12 pigs, 16 chickens, 20 rabbits, 24 eggs
 const cows = loadPictures("cow", 6);
 const pigs = loadPictures("pig", 12);
@@ -28,7 +26,7 @@ class App extends Component {
 
   state = {
     level: 1,
-    picturesArray: cows,
+    picturesArray: pigs,
     chosenArray: [],
     levelScore: 0,
     overallScore: 0,
@@ -36,77 +34,71 @@ class App extends Component {
   }
 
   handleThisClick = event => {
-      let updateChosenArray = this.state.chosenArray;
-      let updatePicturesArray = this.state.picturesArray;
-      let currentScore = this.state.overallScore;
-      let currentLevelScore = this.state.levelScore;
-      //If the clicked picture is not already picked,
-      if (!this.state.chosenArray.includes(event.target.getAttribute("data-id")))
-      {
-        //Push it to the array that collects all pictures that have already been pressed
-        updateChosenArray.push(event.target.getAttribute("data-id"));
-        //Increase the scores
-        currentScore++;
-        currentLevelScore++;
+    let updateChosenArray = this.state.chosenArray;
+    let updatePicturesArray = this.state.picturesArray;
+    let currentScore = this.state.overallScore;
+    let currentLevelScore = this.state.levelScore;
+    //If the clicked picture is not already picked,
+    if (!this.state.chosenArray.includes(event.target.getAttribute("data-id"))) {
+      //Push it to the array that collects all pictures that have already been pressed
+      updateChosenArray.push(event.target.getAttribute("data-id"));
+      //Increase the scores
+      currentScore++;
+      currentLevelScore++;
 
-        //Update the state with the chosen array and the current score.
+      //Update the state with the chosen array and the current score.
+      this.setState({
+        chosenArray: updateChosenArray,
+        overallScore: currentScore,
+        levelScore: currentLevelScore
+      })
+
+      this.randomizeArray(updatePicturesArray);
+      console.log(currentLevelScore);
+      console.log(currentScore);
+
+      //If the levels' score is equal to the array length, user has won.
+      if (currentLevelScore === updatePicturesArray.length) {
+        console.log("You win!")
+        //Update level, change array of pictures
+        let updateLevel = this.state.level + 1;
+        this.resetGame(updateLevel, currentScore);
+      }
+    }
+    else {
+      alert("Oh no! The alien had abducted an animal! Game over! Try again.");
+      if (this.state.highScore < currentScore) {
         this.setState({
-            chosenArray: updateChosenArray,
-            overallScore: currentScore,
-            levelScore: currentLevelScore
+          highScore: currentScore
         })
-
-        this.randomizeArray(updatePicturesArray);
-        console.log(currentLevelScore);
-        console.log(currentScore);
-
-        //If the levels' score is equal to the array length, user has won.
-        if (currentLevelScore === updatePicturesArray.length)
-        {
-          console.log("You win!")
-          //Update level, change array of pictures
-          let updateLevel = this.state.level+1;
-          this.resetGame(updateLevel, currentScore);
-        }
       }
-      else
-      {
-          alert("Oh no! The alien had abducted an animal! Game over! Try again.");
-          if(this.state.highScore < currentScore)
-          {
-            this.setState({
-              highScore: currentScore
-            }) 
-          }
-          this.resetGame(1, 0);
-      }
-      console.log(this.state.chosenArray);
+      this.resetGame(1, 0);
+    }
+    console.log(this.state.chosenArray);
   }
 
 
-  resetGame(level, runningScore)
-  {
+  resetGame(level, runningScore) {
     let animals = [];
-    switch (level)
-    {
+    switch (level) {
       case 1:
 
         animals = cows;
-        break; 
+        break;
       case 2:
         animals = pigs;
-        break; 
+        break;
       case 3:
         animals = chickens;
-        break; 
+        break;
       case 4:
         animals = rabbits;
         break;
       case 5:
         animals = eggs;
-        break;  
-      default: 
-          animals = cows;
+        break;
+      default:
+        animals = cows;
     }
     this.setState({
       level: level,
@@ -118,16 +110,14 @@ class App extends Component {
   }
 
 
-  randomizeArray(array)
-  {
+  randomizeArray(array) {
     //I learned how to shuffle arrays here: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
     let j, x, i;
-    for (i = array.length - 1; i > 0; i--)
-    {
-        j = Math.floor(Math.random() * (i + 1));
-        x = array[i];
-        array[i] = array[j];
-        array[j] = x;
+    for (i = array.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = array[i];
+      array[i] = array[j];
+      array[j] = x;
     }
   }
 
@@ -135,20 +125,20 @@ class App extends Component {
   render() {
     return (
       <div className="overall-wrap">
-        <div className = "inner-wrap text-center">
-        <div id="alien"><img src="./images/AlienSprite.png" /></div>
-          <Header 
-          level={this.state.level}
+        <div className="inner-wrap text-center">
+          <div id="alien"><img src="./images/AlienSprite.png" alt="alien" /></div>
+          <Header
+            level={this.state.level}
           />
-          <Scores 
-          score= {this.state.overallScore}
-          highScore= {this.state.highScore}
-          /> 
+          <Scores
+            score={this.state.overallScore}
+            highScore={this.state.highScore}
+          />
           {this.state.picturesArray.map(picture => {
-            return(
-              <Pictures 
-              thePics={picture}
-              click={this.handleThisClick}
+            return (
+              <Pictures
+                thePics={picture}
+                click={this.handleThisClick}
               />
             )
           })}
--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Pictures = props => (
-    <div className="pictureDiv col-sm-2" data-id={props.thePics.id}><img className="curve-border" onClick={props.click} data-id={props.thePics.id} src={props.thePics.image} /></div>
+    <div className="pictureDiv col-sm-2" data-id={props.thePics.id}><img className="curve-border" onClick={props.click} data-id={props.thePics.id} src={props.thePics.image} alt={"match" + props.thePics.id} /></div>
 );
 export default Pictures;
